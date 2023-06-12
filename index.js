07090ffb632ebb1b9329c5d1e43c66dbaddf624c
Shazzad Hossen <tipsfire.com@gmail.com>
2023-06-12 20:12:22 +0600

    cors

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ const client = new MongoClient(uri, {
 
   
 
-
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 //Middleware
 app.use(cors());
